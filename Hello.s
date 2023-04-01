037ba9a04cf174f4f6c08078c31855f455041147
Saima Akter <74361691+Saimatonni@users.noreply.github.com>
2023-04-01 19:05:54 +0600

    Update Hello.s

--- Hello.s
+++ Hello.s
@@ -1,1 +1,20 @@
-
+SWI_WriteC EQU &0
+SWI_Exit EQU &11
+	
+       AREA MYCODE,CODE
+     	    ENTRY
+			EXPORT __main
+			
+__main
+       ADR r1,TEXT
+	   
+LOOP LDRB r0, [r1],#1
+          CMP r0,#0
+		  SWINE SWI_WriteC
+		  BNE LOOP
+		  SWI SWI_Exit
+TEXT  = "Hello, World!",&0a,&0d,0
+	 
+stop B stop
+    
+	 END
